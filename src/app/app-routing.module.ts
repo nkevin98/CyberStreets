696f18a318a1396f08ccf50b1432e5f116105bce
patelraghav <blackdiamondsftw@gmail.com>
2022-04-07 11:39:41 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', redirectTo: '/navigate', pathMatch: 'full'},
+                        //{path: 'navigate', component: AppComponent},
+                        {path: "signUp", component: RegisterComponent},
+                        {path: "signIn", component: LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
